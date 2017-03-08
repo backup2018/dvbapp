decd47c0ebf00e21ce3ec3c42e63cb6ce4b436e5
openspa <openspateam@gmail.com>
2017-03-08 14:57:37 +0100

    [servicedvb] Fix tab

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -2913,12 +2913,12 @@ void eDVBServicePlay::updateDecoder(bool sendSeekableStateChanged)
 		selectAudioStream();
 
 #if HAVE_AMLOGIC
-			m_decoder->setSyncPCR(pcrpid);
+		m_decoder->setSyncPCR(pcrpid);
 #else
-			if (!(m_is_pvr || m_is_stream || m_timeshift_active))
-				m_decoder->setSyncPCR(pcrpid);
-			else
-				m_decoder->setSyncPCR(-1);
+		if (!(m_is_pvr || m_is_stream || m_timeshift_active))
+			m_decoder->setSyncPCR(pcrpid);
+		else
+			m_decoder->setSyncPCR(-1);
 #endif
 
 		if (m_decoder_index == 0)
