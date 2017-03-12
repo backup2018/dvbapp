23bac38ee5efb5efa07b0e9027fb87494f7d050d
openspa <openspateam@gmail.com>
2017-03-13 00:02:13 +0100

    atsc: store source_id in eServiceReferenceDVB
    we need the source_id to match events in the EIT

--- lib/dvb/idvb.h
+++ lib/dvb/idvb.h
@@ -201,6 +201,9 @@ struct eServiceReferenceDVB: public eServiceReference
 	eTransportStreamID getParentTransportStreamID() const { return eTransportStreamID(data[6]); }
 	void setParentTransportStreamID( eTransportStreamID tsid ) { data[6]=tsid.get(); }
 
+	int getSourceID() const { return data[7]; }
+	void setSourceID(int sourceid) { data[7] = sourceid; }
+
 	eServiceReferenceDVB getParentServiceReference() const
 	{
 		eServiceReferenceDVB tmp(*this);
