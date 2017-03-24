b4734c4d5aafd63de995f4a17711f93fdb7dc16f
openspa <openspateam@gmail.com>
2017-03-24 17:53:07 +0100

    Going to paused improvement slower stb's.
     Slower stb's may fail in e2 event update
     when going to paused.
     Some clean ups.
    
    	modified:   lib/service/servicemp3.cpp
    	modified:   lib/service/servicemp3.h

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -513,6 +513,7 @@ eServiceMP3::eServiceMP3(eServiceReference ref):
 	//m_use_last_seek = false;
 	m_last_seek_count = -10;
 	m_seeking_or_paused = false;
+	m_to_paused = false;
 #endif
 	m_useragent = "Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenSPA;;;)";
 	m_extra_headers = "";
@@ -1093,20 +1094,25 @@ RESULT eServiceMP3::seekToImpl(pts_t to)
 		return -1;
 	}
 #endif
-
-	if (m_paused)
-	{
 #if GST_VERSION_MAJOR >= 1
+	if (m_paused || m_to_paused)
+	{
 		m_last_seek_count = 0;
-#endif
 		m_event((iPlayableService*)this, evUpdatedInfo);
 	}
+#else
+	if (m_paused)
+		m_event((iPlayableService*)this, evUpdatedInfo);
+#endif
 #if GST_VERSION_MAJOR >= 1
 	//eDebug("[eServiceMP3] seekToImpl DONE position %" G_GINT64_FORMAT, (gint64)m_last_seek_pos);
 	if (!m_paused)
 	{
-		m_seeking_or_paused = false;
-		m_last_seek_count = 1;
+		if (!m_to_paused)
+		{
+			m_seeking_or_paused = false;
+			m_last_seek_count = 1;
+		}
 	}
 #endif
 	return 0;
@@ -1143,16 +1149,15 @@ RESULT eServiceMP3::trickSeek(gdouble ratio)
 	if (ratio > -0.01 && ratio < 0.01)
 	{
 #if GST_VERSION_MAJOR >= 1
+		//m_last_seek_count = 0;
 		pos_ret = getPlayPosition(pts);
+		m_to_paused = true;
 #else
 		pos_ret = getPlayPosition(pts);
 #endif
 		gst_element_set_state(m_gst_playbin, GST_STATE_PAUSED);
-#if GST_VERSION_MAJOR >= 1
-		m_seeking_or_paused = true;
-		m_last_seek_count = 0;
-#endif
-		if (pos_ret >= 0)
+		//m_paused = true;
+		if ( pos_ret >= 0)
 			seekTo(pts);
 		/* pipeline sometimes block due to audio track issue off gstreamer.
 		If the pipeline is blocked up on pending state change to paused ,
@@ -1172,6 +1177,9 @@ RESULT eServiceMP3::trickSeek(gdouble ratio)
 				seekTo(pts);
 			}
 		}
+#if GST_VERSION_MAJOR >= 1
+		//m_last_seek_count = 0;
+#endif
 		return 0;
 	}
 
@@ -1207,6 +1215,7 @@ RESULT eServiceMP3::trickSeek(gdouble ratio)
 		if (!strcmp(name, "filesrc") || !strcmp(name, "souphttpsrc"))
 		{
 			/* previous state was already ok if we come here just give all elements time to unpause */
+			m_to_paused = false;
 			gst_element_set_state(m_gst_playbin, GST_STATE_PLAYING);
 			ret = gst_element_get_state(m_gst_playbin, &state, &pending, 2 * GST_SECOND);
 #if GST_VERSION_MAJOR >= 1
@@ -1261,6 +1270,7 @@ seek_unpause:
 #if GST_VERSION_MAJOR >= 1
 		m_seeking_or_paused = false;
 		m_last_seek_count = 0;
+		m_to_paused = false;
 #endif
 	}
 
@@ -1497,7 +1507,11 @@ RESULT eServiceMP3::getPlayPosition(pts_t &pts)
 	if ((pos = get_pts_pcrscr()) > 0)
 		pos *= 11111LL;
 #else
+#if GST_VERSION_MAJOR < 1
 	if ((dvb_audiosink || dvb_videosink) && !m_paused && !m_sourceinfo.is_hls)
+#else
+	if ((dvb_audiosink || dvb_videosink) && !m_paused && !m_seeking_or_paused && !m_sourceinfo.is_hls)
+#endif
 	{
 		if (m_sourceinfo.is_audio)
 		{
@@ -1539,7 +1553,14 @@ RESULT eServiceMP3::getPlayPosition(pts_t &pts)
 		if (!gst_element_query_position(m_gst_playbin, fmt, &pos))
 		{
 			//eDebug("[eServiceMP3] gst_element_query_position failed in getPlayPosition");
-			return -1;
+			if (m_last_seek_pos > 0)
+			{
+				pts = m_last_seek_pos;
+				m_last_seek_count = 0;
+				return 0;
+			}
+			else
+				return -1;
 		}
 #endif
 	}
@@ -1973,23 +1994,11 @@ RESULT eServiceMP3::selectTrack(unsigned int i)
 		if (ppos < 0)
 			ppos = 0;
 	}
-//#if GST_VERSION_MAJOR < 0
 	if (validposition)
 	{
 		//flush
 		seekTo(ppos);
 	}
-//#else
-	/* //only flush if the audio types are not the same
-    if (m_audioStreams[m_currentAudioStream].type != m_audioStreams[i].type)
-	{
-		if (validposition)
-		{
-			//flush
-			seekTo(ppos);
-		}
-	}
-#endif*/
 	return selectAudioStream(i);
 }
 
@@ -3217,7 +3226,10 @@ RESULT eServiceMP3::enableSubtitles(iSubtitleUser *user, struct SubtitleTrack &t
 
 #if GST_VERSION_MAJOR >= 1
 		if (m_last_seek_pos > 0)
+		{
 			seekTo(m_last_seek_pos);
+			gst_sleepms(50);
+		}
 #endif
 
 	}
