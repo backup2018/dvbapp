30609e628994128a5649e4aca4a7f87111152af5
openspa <openspateam@gmail.com>
2017-05-03 00:18:07 +0200

    [Standby] Update screen.

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -237,7 +237,7 @@ class QuitMainloopScreen(Screen):
 	def __init__(self, session, retvalue=1):
 		self.skin = """<screen name="QuitMainloopScreen" position="fill" flags="wfNoBorder">
 				<ePixmap pixmap="skin_default/icons/input_info.png" position="c-27,c-60" size="53,53" alphatest="on" />
-				<widget name="text" position="center,c+5" size="720,100" font="Regular;22" halign="center" />
+				<widget name="text" position="center,c+5" size="720,150" font="Regular;22" halign="center" />
 			</screen>"""
 		Screen.__init__(self, session)
 		from Components.Label import Label
