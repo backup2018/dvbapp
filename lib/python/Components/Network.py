6b629782d038607717887ac1952699cddc241a84
openspa <openspateam@gmail.com>
2017-05-08 23:21:47 +0200

    spycat4k add backup/flashonline/multiboot options

--- lib/python/Components/Network.py
+++ lib/python/Components/Network.py
@@ -42,7 +42,7 @@ class Network:
 		return self.remoteRootFS
 
 	def isBlacklisted(self, iface):
-		return iface in ('lo', 'wifi0', 'wmaster0', 'sit0', 'tun0', 'tap0', 'sys0')
+		return iface in ('lo', 'wifi0', 'wmaster0', 'sit0', 'tun0', 'tap0', 'sys0', 'p2p0')
 
 	def getInterfaces(self, callback = None):
 		self.configuredInterfaces = []
@@ -305,7 +305,7 @@ class Network:
 		moduledir = self.getWlanModuleDir(iface)
 		if moduledir:
 			name = os.path.basename(os.path.realpath(moduledir))
-			if name in ('ath_pci','ath5k'):
+			if name in ('ath_pci','ath5k','ar6k_wlan'):
 				name = 'Atheros'
 			elif name in ('rt73','rt73usb','rt3070sta'):
 				name = 'Ralink'
