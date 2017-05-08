0529efc094bb11ff410afad4bbcbb90d4a9068de
openspa <openspateam@gmail.com>
2017-05-08 23:04:41 +0200

    prepare spycat4k

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -91,7 +91,7 @@ def getPythonVersionString():
 		return _("unknown")
 
 def getCPUString():
-	if getMachineBuild() in ('vuuno4k', 'vuultimo4k','vusolo4k', 'hd51', 'hd52', 'sf4008', 'dm900', 'gb7252', 'dags7252', 'vs1500', 'et1x000', 'xc7439','h7'):
+	if getMachineBuild() in ('vuuno4k', 'vuultimo4k', 'vusolo4k', 'hd51', 'hd52', 'sf4008', 'dm900', 'gb7252', 'dags7252', 'vs1500', 'et1x000', 'xc7439','h7'):
 		return "Broadcom"
 	elif getMachineBuild() in ('wetekplay', 'wetekplay2', 'odroidc2'):
 		return "AMLogic"
@@ -118,9 +118,9 @@ def getCPUSpeedString():
 		return "1,2 GHz"
 	elif getMachineBuild() in ('vusolo4k'):
 		return "1,5 GHz"
-	elif getMachineBuild() in ('vuuno4k','vuultimo4k','dm900', 'gb7252', 'dags7252'):
+	elif getMachineBuild() in ('vuuno4k', 'vuultimo4k', 'dm900', 'gb7252', 'dags7252', 'xc7439'):
 		return "1,7 GHz"
-	elif getMachineBuild() in ('hd51','hd52','sf4008','vs1500','et1x000','h7'):
+	elif getMachineBuild() in ('hd51', 'hd52', 'sf4008', 'vs1500', 'et1x000', 'h7'):
 		try:
 			import binascii
 			f = open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb')
