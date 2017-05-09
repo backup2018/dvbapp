afbe31d9743392adbed0b3e39ed6b0e2c36888b0
openspa <openspateam@gmail.com>
2017-05-09 15:11:33 +0200

    [RecordPaths] Change screen size.

--- lib/python/Screens/RecordPaths.py
+++ lib/python/Screens/RecordPaths.py
@@ -13,12 +13,12 @@ config.misc.picon_search_hdd = ConfigYesNo (default = False)
 
 class RecordPathsSettings(Screen,ConfigListScreen):
 	skin = """
-		<screen name="RecordPathsSettings" position="160,150" size="450,290" title="Settings paths">
+		<screen name="RecordPathsSettings" position="160,150" size="450,200" title="Settings paths">
 			<ePixmap pixmap="skin_default/buttons/red.png" position="10,0" size="140,40" alphatest="on" />
 			<ePixmap pixmap="skin_default/buttons/green.png" position="300,0" size="140,40" alphatest="on" />
 			<widget source="key_red" render="Label" position="10,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
 			<widget source="key_green" render="Label" position="300,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
-			<widget name="config" position="10,44" size="430,236" />
+			<widget name="config" position="10,44" size="430,146" />
 		</screen>"""
 
 	def __init__(self, session):
