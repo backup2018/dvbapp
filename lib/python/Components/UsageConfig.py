601befcb5cc522eb814de3015a72f7b24c3b1b3d
openspa <openspateam@gmail.com>
2017-05-15 22:43:29 +0200

    Add menu_sort - thx vti

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -111,13 +111,18 @@ def InitUsageConfig():
 
 	config.usage.sort_settings = ConfigYesNo(default = False)
 	config.usage.sort_menus = ConfigYesNo(default = False)
-	
+
 	config.usage.plugins_sort_mode = ConfigSelection(default = "user", choices = [
 		("a_z", _("alphabetical")),
 		("default", _("Default")),
 		("user", _("user defined")),])
 	config.usage.plugin_sort_weight = ConfigDictionarySet()
-	
+	config.usage.menu_sort_weight = ConfigDictionarySet(default = { "mainmenu" : {"submenu" : {} }})
+	config.usage.menu_sort_mode = ConfigSelection(default = "user", choices = [
+		("a_z", _("alphabetical")),
+		("default", _("Default")),
+		("user", _("user defined")),])
+
 	choicelist = []
 	for i in (10, 30):
 		choicelist.append((str(i), ngettext("%d second", "%d seconds", i) % i))
